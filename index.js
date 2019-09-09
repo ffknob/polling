c2d434e02edec07b1c2ba54072451845c485cd14
ffknob <ffknob@tce.rs.gov.br>
2019-09-09 11:44:07 -0300

    logger database events

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
-require('dotenv').config();
+const morgan = require('morgan');
+const logger = require('./services/logger');
+
+const mongodb = require('./services/mongodb');
 
 const app = express();
 
@@ -14,11 +18,18 @@ app.use(bodyParser.json());
 app.use('/', (req, res, next) => console.log('OK'));
 
 app.use((err, req, res, next) => {
-	console.log(err);
+	logger.error(err);
 	res.status(500).send(`Internal Server Error: ${err}`);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}...`);
-});
+mongodb
+	.connect()
+	.then(connection => {
+		const PORT = process.env.PORT || 3000;
+		app.listen(PORT, () => {
+			logger.info(`Server listening on port ${PORT}...`);
+		});
+	})
+	.catch(err => {
+		logger.error(err);
+	});
---
+++ services/mongodb.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const logger = require('./logger');
+
+const MONGODB_HOST = process.env.MONGODB_HOST || 'locahost';
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME || 'user';
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'password';
+const MONGODB_DATABASE = process.env.MONGODB_DATABASE || 'test';
+
+const CONNECTION_STRING = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+mongoose.connection.on('connecting', () => logger.info('Connecting...'));
+mongoose.connection.on('connected', () => logger.info('Connected'));
+mongoose.connection.on('diconnecting', () => logger.info('Disconnecting...'));
+mongoose.connection.on('diconnected', () => logger.info('Disconnected'));
+mongoose.connection.on('error', () => logger.error('Error'));
+
+exports.connect = () => {
+	return mongoose.connect(CONNECTION_STRING);
+};
